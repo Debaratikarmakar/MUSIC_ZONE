fbe7b6568410dcd8e39f390d9e2ed9edbb0480e1
Debaratikarmakar <87557586+Debaratikarmakar@users.noreply.github.com>
2024-07-12 11:24:07 +0530

    Update DisplayHome.jsx

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -26,6 +26,22 @@ const DisplayHome = () => {
         </div>
      </div>
 
+
+
+
+
+
+
+
+
+      {/* album data input */}
+    <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <div className='flex overflow-auto'>
+        {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+     </div>
+
     </>
   )
 }
